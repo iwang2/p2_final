378b7c771e2e98a4c7b7dcfef8774e069e8d6988
Ida <iwang2@stuy.edu>
2018-01-18 11:01:58 -0500

    asdfasdfjasg

--- art/print_num.c
+++ art/print_num.c
@@ -8,21 +8,45 @@
 
 // 16 lines per number
 // file size of percent is 368
-/*
-char * print_num (int n) {
-  char s[sizeof("0.txt")];
-  sprintf(s, "%d", n);
-  strcat(s, ".txt");
 
-  struct stat sb;
-  stat(s, &sb);
+void print_num (int n) {
+  int
+    fd1 = -1,
+    fd2 = -1;
+  int fdp = open("per.txt", O_RDONLY);
+  int size = 0;
 
-  int fd1 = open(s, O_RDONLY);
-  int fd2 = open("per.txt", O_RDONLY);  
+  if ( n/10 > 0 ) {
+    
+  }
+  
+  for (int i = 0; n / 10 > 0; i++) {    
+    char s[sizeof("per.txt")];
+    if (nc == 1) {
+      strcpy(s, "per.txt");
+    } else {
+      sprintf(s, "%d", n%10);
+      strcat(s, ".txt");
+    }
+    struct stat sb;
+    stat(s, &sb);
+    
+    size = (int)sb.st_size / 16;
+
+    fd = open(s, O_RDONLY);
+
+    n /= 10;
+  }
+
+  for (int i = 0; i < 
+
+  close(fd1);
+  close(fdp);
+  if (fd2 > -1) close(fd2);
 }
-*/
 
-char ** arr () {
+/*
+char *** arr () {
   char *** s = malloc(10 * 16 * sizeof(char *));
   //char s[10][16][23];
   char temp[sizeof("0.txt")];
@@ -49,6 +73,7 @@ char ** arr () {
   }
   return s;
 }
+*/
 
 int main(){
   for(int i = 0; i < 11; i++){
