20684ff77e345092e5fb6ed5d0762be81aba80ec
iwang2 <iwang2@stuy.edu>
2018-01-18 22:59:31 -0500

    PRINTING NUMBERS WORKKKSDASDFDASDFASDSGA

--- art/print_num.c
+++ art/print_num.c
@@ -10,8 +10,13 @@
 // file size of percent is 368
 
 void print_num (int n) {
+  int fd, size;
+  
   int fds[3][2];
   fds[2][0] = open("per.txt", O_RDONLY);
+  struct stat sb;
+  stat("per.txt", &sb);
+  fds[2][1] = (int)sb.st_size / 16;
   // fds[x][0] is the file descriptor
   // fds[x][1] is the line size
 
@@ -21,7 +26,6 @@ void print_num (int n) {
     strcat(s, ".txt");
     fds[g][0] = open(s, O_RDONLY);
 
-    struct stat sb;
     stat(s, &sb);
     fds[g][1] = (int)sb.st_size / 16;
     
@@ -30,91 +34,24 @@ void print_num (int n) {
   
   for (int i = 0; i < 16; i++) {
     for(int j = 0; j < 3; j++) {
-      int fd = fds[j][0], size = fds[j][1];
+      fd = fds[j][0];
+      size = fds[j][1];
+      
       char buff[size];
       read(fd, buff, size);
+      buff[0] = ' ';
+      buff[size-1] = 0;
       printf("%s ", buff);
+      //memset(buff, 0, sizeof(buff));
     }
     printf("\n");
   }
 
-  /*
-  for (int i = 0; n / 10 > 0; i++) {    
-    char s[sizeof("per.txt")];
-    if (nc == 1) {
-      strcpy(s, "per.txt");
-    } else {
-      sprintf(s, "%d", n%10);
-      strcat(s, ".txt");
-    }
-    struct stat sb;
-    stat(s, &sb);
-    
-    size = (int)sb.st_size / 16;
-
-    fd = open(s, O_RDONLY);
-
-    n /= 10;
+  for (int k = 0; k < 3; k++) {
+    close(fds[k][0]);
   }
-
-  for (int i = 0; i < 
-
-  close(fd1);
-  close(fdp);
-  if (fd2 > -1) close(fd2);
-  */
 }
-
-/*
-char *** arr () {
-  char *** s = malloc(10 * 16 * sizeof(char *));
-  //char s[10][16][23];
-  char temp[sizeof("0.txt")];
-  
-  for(int i = 0; i < 10; i++){
-    sprintf(temp, "%d", i);
-    strcat(temp, ".txt");
-
-    struct stat sb;
-    stat(temp, &sb);
-    int size = (int)sb.st_size / 16;
-
-    int fd = open(temp, O_RDONLY);
-    
-    for(int n = 0; n < 16; n++){
-      //s[i][n] = (char *)malloc(size);
-      *(s + i + n) = malloc(size);
-      if (read(fd, *(s + i + n), size) == -1) {
-	printf("%s\n", strerror(errno));
-	exit(0);
-      }
-    }
-    close(fd);
-  }
-  return s;
-}
-*/
 
 int main(){
-  for(int i = 0; i < 11; i++){
-    struct stat sb;
-    char s[sizeof("abc.txt")];
-    
-    if (i == 10) strcpy(s, "per");
-    else sprintf(s, "%d", i);
-
-    strcat(s, ".txt");
-    stat(s, &sb);
-    printf("%s: %d\n", s, (int)sb.st_size);
-  }
-  
-  char *** s = arr();
-  //strcpy(s, arr());
-  for(int i = 0; i < 10; i++){
-    for(int k = 0; k < 16; k++){
-      //for(int j = 0; a[j]; j++){
-      printf("%d, %d: %s\n", i, k, *(s + i + k));
-	// }
-    }
-  }
+  print_num(87);
 }
