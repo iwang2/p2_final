6fd97fffe5b6a2d760ca3c14229c30a02cbb9d4b
iwang2 <iwang2@stuy.edu>
2018-01-24 15:34:00 -0500

    server now sends stuff to client

--- networking/client.c
+++ networking/client.c
@@ -1,5 +1,7 @@
 #include "networking.h"
 
+void process(char *s);
+
 int main(int argc, char **argv) {
 
   int server_socket;
@@ -9,13 +11,22 @@ int main(int argc, char **argv) {
     server_socket = client_setup( argv[1]);
   else
     server_socket = client_setup( TEST_IP );
-
-  while (1) {
-    printf("enter data: ");
-    fgets(buffer, sizeof(buffer), stdin);
-    *strchr(buffer, '\n') = 0;
-    write(server_socket, buffer, sizeof(buffer));
-    read(server_socket, buffer, sizeof(buffer));
+  
+  while ( read(server_socket, buffer, sizeof(buffer)) ) {
     printf("received: [%s]\n", buffer);
+    process(buffer);
+    write(server_socket, buffer, sizeof(buffer));
+  }
+  exit(0);
+}
+
+void process(char * s) {
+  while (*s) {
+    if (*s >= 'a' && *s <= 'z')
+      *s = ((*s - 'a') + 13) % 26 + 'a';
+    else  if (*s >= 'A' && *s <= 'Z')
+      *s = ((*s - 'a') + 13) % 26 + 'a';
+    s++;
   }
 }
+
