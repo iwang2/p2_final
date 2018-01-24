f545542533f808ca38f740660c2226d798d0eaee
Alex Lu <Alex@Alexs-MacBook-Pro-2.local>
2018-01-24 16:28:45 -0500

    ??

--- networking/algo.c
+++ networking/algo.c
@@ -5,8 +5,8 @@ int* assign(int k){
   srand(time(NULL));
   int i;
   int track = 100;
-  int * probs = calloc(k, sizeof(int));
-  for(i = 0; i < k - 1; i++){
+  int * probs = calloc(k + 1, sizeof(int));
+  for(i = 1; i < k; i++){
     int temp = (rand() % track);
     if(track > 0){      
       probs[i] = temp;
@@ -16,8 +16,8 @@ int* assign(int k){
       probs[i] = 0;
     }
   }
-  probs[k - 1] = track;
-  //probs[k] = k;
+  probs[k] = track;
+  probs[0] = k;
   return probs;
 }
 
@@ -26,29 +26,30 @@ int arrlast(int * arr){
   int i;
   for(i = 0; arr[i]; i++
 }
-*/
+
 int arrlen(int * arr){
   return (sizeof(arr) / sizeof(arr[0]));
 }
+*/
 
 int pick(int * probs){
   int * weighted = calloc(100, sizeof(int));
   int i;
   int k;
   srand(time(NULL));
-  for(i = 0; i < arrlen(probs); i++){
+  for(i = 1; i < 1 + probs[0]; i++){
     for(k = 0; k < probs[i]; k++){
-      weighted[k] = i;
+      weighted[k] = i - 1;
     }
   }
   
   //test
   int j;
-  /*
+  
   for(j = 0; j < 100; j++){
     printf("At position %d, the value of the weighted array is %d\n", j, weighted[j]);
   }
-  */
+  
   int ret = weighted[rand() % 100];
   free(weighted);
   return ret;
@@ -60,9 +61,11 @@ int pick(int * probs){
 int main(){
   int j = 5;
   int * test = assign(5);
+  /*
   printf("size of test: %d\n", sizeof(*test));
   printf("size of first element of test: %d\n", sizeof(test[0]));
   printf("length of test: %d\n", (sizeof(test) / sizeof(test[0])));
+  */
   int i;
   for(i = 0; i < j; i++){
     printf("At position %d, the value is %d\n", i, test[i]);
