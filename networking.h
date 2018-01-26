021c5d832c07549297846ef9ce2761465c2b97c8
Penn <penn.z.wu@gmail.com>
2018-01-25 23:24:57 -0500

    Think it's pretty much done besides music

--- networking.h
+++ networking.h
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
--- select_client.c
+++ select_client.c
@@ -18,12 +18,27 @@ int main(int argc, char **argv) {
       printf("This is %s\n", buffer);
     }
     else if (strcmp(buffer, "die") == 0) {
-     printf("I HAVE BEEN KILLED\n");
+     //printf("I HAVE BEEN KILLED\n");
+     char *args[3];
+     args[0] = "cat";
+     args[1] = "dead.txt";
+     args[2] = 0;
+     execvp(args[0], args);
    }
     else {
-      printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-      print_num(atoi(buffer));
+      if (fork() == 0) {
+        char *args[2];
+        args[0] = "clear";
+        args[1] = 0;
+        execvp(args[0], args);
+      }
+      else {
+        int status;
+        wait(&status);
+        print_num(atoi(buffer));
+      }
       //printf("%s\n", buffer);
     }
   }//end loop
+  printf("YOU WIN!!!\n");
 }
