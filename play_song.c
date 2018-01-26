c7f03563178dfc3f36d0db9f1456fddfe33e588e
iwang2 <iwang2@stuy.edu>
2018-01-26 00:36:00 -0500

    got the website code for playsong

--- play_song.c
+++ play_song.c
@@ -1,63 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <string.h>
-#include <time.h>
-#include <errno.h>
-#include <signal.h>
 
-int main(int argc, char * argv[]){
-  int k = atoi(argv[1]);
-  char * args[3];
-  args[0] = "mpg123";
-  args[1] = "song1.mp3";
-  args[2] = NULL;
-
-
-  int pip1[2];
-  pipe(pip1);
-
-  int f = fork();
-
-
-  if(f == 0){ //child
-    close(pip1[0]);
-
-    int k = (int)getpid();
-    
-    write(pip1[1], &k, sizeof(k));
-
-    execvp(args[0], args);
+int main(){
+  pid_t f;
+  char k[20] = "kill -s 9 ";
+ 
+  f = fork();          
+  if (f < 0) {  // just in case fork fails 
+    puts("fork failure");
+    exit(-1);
+  }   
+  else if (f == 0) { // child process 
+    execlp("mpg123", "mpg123", "-q", "song1.mp3", (char *)NULL); 
   }
-  
-  if(f){ //parent
-    close(pip1[1]);
-
-    int j;
-    read(pip1[0], &j, sizeof(j));
-
-    //printf("Child pid: %d\n", j);
-
-    wait(k);
-    kill(j, 9);
-
-    //printf("got past kill\n");
+  else {
+    printf("Press ENTER to quit.\n");
+    sprintf(k, "%s%d", k, f);
+    getchar();  // wait for user input
+    system(k);
   }
-  /*
-  
-  char * kill[2];
-  kill[0] = "q";
-  kill[1] = NULL;
-  
-  if(!fork()){
-    printf("q\n");
-  }
-  */
-  return 17;
-
-    
+  printf("All done.\n");
+  exit(0);
 }
 
 
